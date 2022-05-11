1a6eaeb876faaef1c1951d7c014b5e04e6e0fbc9
Everton Alves <alveseverton023@gmail.com>
2022-05-10 21:26:31 -0300

    template strings

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello word")
+let nome = 'José';
+let sobrenome = 'da Silva';
+let idade = 27;
+let peso = 83.5;
+let altura = 1.7;
+let temPlano = true;
+let imc = peso/(altura**2);
+
+//console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' e seu índice de massa corporal é de ' + imc.toFixed(2));
+
+//template strings
+console.log(`${nome} ${sobrenome} tem ${idade} anos e seu indice de massa corporal é de ${imc.toFixed(2)}`)
